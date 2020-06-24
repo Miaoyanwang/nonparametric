a94f6f3db169cc830c8e7587904880243acfb898
miaoyan wang <saplingwang@gmail.com>
2020-06-23 19:26:43 -0500

    update

--- figure/example/plot.R
+++ figure/example/plot.R
@@ -1,15 +1,16 @@
-## written by Miaoyan Wang, 2020
+## created by Miaoyan Wang 07/2019; modified on 06/2020
 library(rgl)
 library("RColorBrewer")
-library(viridis) ## use more refined color scale
-##marker = list(color = brewer.pal(9, "RdBu"))$color
+##library(viridis) ## use more refined color scale
+marker = list(color = brewer.pal(9, "RdBu"))$color
 
 ### function to plot 3d array
 plot_tensor=function(tensor){
     
 n=prod(dim(tensor))   
-color_choice=min(round(prod(dim(tensor))/(6*6*6)),100)+1
-marker=viridis_pal(option = "B")(color_choice)
+#color_choice=min(round(prod(dim(tensor))/(6*6*6)),100)+1
+#marker=viridis_pal(option = "B")(color_choice)
+color_choice=round(prod(dim(tensor))/(20*20*20))+1
 
 position=positionfun(dim(tensor))$position
 quan=c(quantile(tensor,(0:color_choice)/color_choice))
@@ -44,15 +45,15 @@ cluster1_index=sample(1:K1,d1,replace=T) ## randomly assign d1 rows to K1 cluste
 cluster2_index=sample(1:K2,d2,replace=T) 
 cluster3_index=sample(1:K3,d3,replace=T)
 
-K1=length(unique(cluster1_index)) ## actual number of clusters; must be no larger than K1
+K1=length(unique(cluster1_index)) ## actual number of clusters in realization; no larger than pre-specified K1
 K2=length(unique(cluster2_index))
 K3=length(unique(cluster3_index))
 
 group_mean=array(runif(K1*K2*K3,-3,3),dim=c(K1,K2,K3)) ## generate group-specific mean
-signal=group_mean[cluster1_index,cluster2_index,cluster3_index] ## generate a tensor following the groups-spedific mean
+signal=group_mean[cluster1_index,cluster2_index,cluster3_index] ## generate a stochastic block tensor following the groups-spedific mean
+noise=array(rnorm(d1*d2*d3,0,0.1),dim=c(d1,d2,d3))
 
-
-plot_tensor(signal) ## plot data tensor
+plot_tensor(signal+noise) ## plot data tensor
 signal_reorganize=signal[sort(cluster1_index,index=T)$ix,sort(cluster2_index,index=T)$ix,sort(cluster3_index,index=T)$ix] ## re-organize the tensor entries; put entries in the same groups together.
-plot_tensor(signal_reorganize) ## plot the data tensor with reorganized indices
+plot_tensor(signal_reorganize) ## plot the signal tensor with reorganized indices
 
