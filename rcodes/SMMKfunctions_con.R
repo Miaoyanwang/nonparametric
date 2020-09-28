c233e98d2c04c465330544c13c4eefb1997992dc
miaoyan wang <saplingwang@gmail.com>
2020-09-28 00:00:32 -0500

    update

--- rcodes/SMMKfunctions_con.R
+++ rcodes/SMMKfunctions_con.R
@@ -29,27 +29,6 @@ Karray = function(X,kernel = function(X1,X2) t(X1)%*%X2, type="col"){
   return(as.tensor(K))
 }
 
-# K = function(A,B,kernel = function(a,b) sum(a*b),type = c("row","col")){
-#   if(nrow(A) != nrow(B)) stop("two matrix sizes are different")
-#   if(ncol(A) != ncol(B)) stop("two matrix sizes are different")
-#   d1 = nrow(A); d2 = ncol(A)
-#   if (type == "row") {
-#     Kab = matrix(nrow= d1,ncol = d1)
-#     for(i in 1:d1){
-#       for(j in 1:d1){
-#         Kab[i,j] = kernel(A[i,],B[j,])
-#       }
-#     }
-#   }else{
-#     Kab = matrix(nrow= d2,ncol = d2)
-#     for(i in 1:d2){
-#       for(j in 1:d2){
-#         Kab[i,j] = kernel(A[,i],B[,j])
-#       }
-#     }
-#   }
-#   return(Kab)
-# }
 
 expkernel = function(Y,Z){
   n = ncol(Y)
@@ -72,7 +51,7 @@ linearkernel = function(X1,X2) t(X1)%*%X2
 
 constkernel = function(X1,X2) return(matrix(0,nrow=ncol(X1),ncol=ncol(X1)))
 
-SMMK_con = function(X,y,r,kernel_row = c("linear","poly","exp","const"),kernel_col = c("linear","poly","exp","const"), cost = 10, rep = 1, p = .5){
+SMMK_con = function(X,y,r,kernel_row = c("linear","poly","exp","const"),kernel_col = c("linear","poly","exp","const"), cost = 10, rep = 1, p = .5,sparse=0){
   result = list()
   
   # Default is linear kernel.
@@ -104,7 +83,6 @@ SMMK_con = function(X,y,r,kernel_row = c("linear","poly","exp","const"),kernel_c
   compareobj = 10^10
   
   
-  
   for(nsim in 1:rep){
     error = 10; iter = 0; obj = 10^10
     # initialize P_row,P_col
@@ -115,14 +93,6 @@ SMMK_con = function(X,y,r,kernel_row = c("linear","poly","exp","const"),kernel_c
     while((iter < 20)&(error >10^-3)){
       # update C
       W_row = P_row%*%t(P_row); W_col = P_col%*%t(P_col)
-      #Dmat = matrix(nrow = n,ncol = n)
-      #for(i in 1:n){
-      #for(j in 1:n){
-      #     Dmat[i,j] = sum(W_row*K_row[i,j,,])+sum(W_col*K_col[i,j,,])
-      #  }
-      #}
-      ## loop is slow. Replace loop by tensor-matrix multiplication
-      
       Dmat=matrix(unfold(K_row,c(1,2),c(3,4))@data%*%c(W_row)+unfold(K_col,c(1,2),c(3,4))@data%*%c(W_col),nrow=n,ncol=n)
       
       dvec = rep(1,n)
@@ -131,33 +101,6 @@ SMMK_con = function(X,y,r,kernel_row = c("linear","poly","exp","const"),kernel_c
       bvec = c(rep(0,1+n),ifelse(y==1,-cost*(1-p),-cost*p))
       res = solve.QP(Dmat,dvec,Amat,bvec,meq =1)
       alpha=res$solution
-      
-      # update P
-      #CC_row = 0; CPh_row = array(0,dim = c(n,r,d1))
-      #CC_col = 0; CPh_col = array(0,dim = c(n,r,d2))
-      #for(i in 1:n){
-      # for(j in 1:n){
-      #   CC_row = CC_row + y[i]*alpha[i]*y[j]*alpha[j]*K_row[i,j,,]
-      #   CC_col = CC_col + y[i]*alpha[i]*y[j]*alpha[j]*K_col[i,j,,]
-      # }
-      #}
-      
-      ## no need to find explicite inverse
-      #CCi_row = solve(t(P_row)%*%CC_row%*%P_row)
-      #CCi_col = solve(t(P_col)%*%CC_col%*%P_col)
-      
-      ## CPh_row is an intermediate step for CC_row. No need to compute twice.
-      #for(i in 1:n){
-      # cph_row = 0; cph_col = 0;
-      # for(j in 1:n){
-      #   cph_row = cph_row + y[j]*alpha[j]*K_row[j,i,,]
-      #   cph_col = cph_col + y[j]*alpha[j]*K_col[j,i,,]
-      # }
-      # CPh_row[i,,] = t(P_row)%*%cph_row
-      # CPh_col[i,,] = t(P_col)%*%cph_col
-      #}
-      
-      
       
       CPh_row=ttl(K_row,list(t(as.matrix(y*alpha)),t(P_row)),ms=c(1,3))
       CPh_col=ttl(K_col,list(t(as.matrix(y*alpha)),t(P_col)),ms=c(1,3))
@@ -172,16 +115,6 @@ SMMK_con = function(X,y,r,kernel_row = c("linear","poly","exp","const"),kernel_c
       factor_col=unfold(ttm(CPh_col,sqrtm(Makepositive(CC_col))$Binv,3),2,c(1,3,4))@data
       Dmat=factor_row%*%t(factor_row)+factor_col%*%t(factor_col)
       
-      #Dmat = matrix(nrow = n,ncol = n)
-      #for(i in 1:n){
-      # for(j in 1:n){
-      #   Dmat[i,j] = sum(CCi_row%*%CPh_row[i,,]*CPh_row[j,,])+
-      #                            sum(CCi_col%*%CPh_col[i,,]*CPh_col[j,,])
-      # }
-      #}
-      ### Use Gram matrix to efficiently compute Dmat
-      
-      
       
       dvec = rep(1,n)
       Dmat = Makepositive((y%*%t(y))*Dmat)
@@ -194,15 +127,6 @@ SMMK_con = function(X,y,r,kernel_row = c("linear","poly","exp","const"),kernel_c
       error = abs(-obj[iter+1]+obj[iter])/obj[iter]
       
       # P formula
-      #P_row = 0; P_col = 0
-      #for(i in 1:n){
-      # P_row = P_row + alpha[i]*y[i]*rbind(CPh_row[i,,])
-      # P_col = P_col + alpha[i]*y[i]*rbind(CPh_col[i,,])
-      #}
-      
-      #P_row = svd(t(P_row)%*%CCi_row)$u; P_col = svd(t(P_col)%*%CCi_col)$u
-      ## No need to multiply CCi_row. Right multiplying a full-rank matrix will not change the left singular vector
-      
       P_row=ttm(CPh_row,t(as.matrix(alpha*y)),2)[1,1,,]@data
       P_col=ttm(CPh_col,t(as.matrix(alpha*y)),2)[1,1,,]@data
       
@@ -211,6 +135,17 @@ SMMK_con = function(X,y,r,kernel_row = c("linear","poly","exp","const"),kernel_c
       
       P_row = svd(P_row)$v
       P_col = svd(P_col)$v
+      
+      #### sparse model
+      if(sparse>=1){
+          B=0
+          for(i in 1:n){
+              B=B+alpha[i]*y[i]*P_row%*%t(P_row)%*%X[[i]]%*%P_col%*%t(P_col)
+          }
+        B=sparse_matrix(B,r,sparse,sparse)
+        P_row=svd(B)$u[,1:r]
+        P_col=svd(B)$v[,1:r]
+      }
       
     }
     if(compareobj>obj[iter+1]){
@@ -221,15 +156,10 @@ SMMK_con = function(X,y,r,kernel_row = c("linear","poly","exp","const"),kernel_c
   }
   
   
-  ## move outside the loop. Only need to compute once for the optimal replicate.
+
   P_row= P_row_optimum; P_col= P_col_optimum;
   W_row = P_row%*%t(P_row); W_col = P_col%*%t(P_col)
-  #Dmat = matrix(nrow = n,ncol = n)
-  #for(i in 1:n){
-  # for(j in 1:n){
-  #   Dmat[i,j] = sum(W_row*K_row[i,j,,])+sum(W_col*K_col[i,j,,])
-  # }
-  #}
+
   Dmat=K=matrix(unfold(K_row,c(1,2),c(3,4))@data%*%c(W_row)+unfold(K_col,c(1,2),c(3,4))@data%*%c(W_col),nrow=n,ncol=n)
   
   dvec = rep(1,n)
@@ -253,27 +183,27 @@ SMMK_con = function(X,y,r,kernel_row = c("linear","poly","exp","const"),kernel_c
   }
   
   # intercept part estimation (update b)
-  #positive = min(unlist(lapply(X,slope))[which(y==1)])
-  #negative = max(unlist(lapply(X,slope))[which(y==-1)])
   
   yfit=K%*%(alpha*y) ## faster than lapply
   positive=min(yfit[y==1])
   negative=max(yfit[y==-1])
-  #intercept = -(positive+negative)/2
   if ((1-positive)<(-1-negative)) {
       intercept = -(positive+negative)/2
-  }else{
-  gridb0 = seq(from = -1-negative,to = 1-positive,length = 100)
-  intercept = gridb0[which.min(sapply(gridb0,function(b) objective(b,yfit,y,p = p)))]
+      }else{
+      gridb0 = seq(from = -1-negative,to = 1-positive,length = 100)
+      intercept = gridb0[which.min(sapply(gridb0,function(b) objective(b,yfit,y,p = p)))]
   }
   compareobj = obj[iter+1]
   predictor = function(Xnew) sign(slope(Xnew)+intercept)
   
   result$alpha = alpha
   result$slope = slope; result$predict = predictor
-  result$intercept = intercept
-  result$P_row = P_row; result$P_col = P_col
-  result$obj = obj[-1]; result$iter = iter; result$error = error; result$fitted=yfit+intercept ## add fitted value as a criterium to select cost
+  result$intercept = intercept;
+  result$P_row = P_row; result$P_col = P_col;
+  result$obj = obj[-1]; result$iter = iter;
+  result$error = error;
+  result$fitted=yfit+intercept; ## add fitted value as a criterium to select cost
+  result$B=B
   return(result)
 }
 
@@ -282,5 +212,93 @@ hinge = function(y) ifelse(1-y>0,1-y,0)
 objective=function(b,yfit,y,p){
     return(sum(hinge(y*(yfit+b))[y==1]*(1-p))+sum(hinge(y*(yfit+b))[y==-1]*p))
 }
-                  
-                  
+
+sparse_matrix=function(M,r,srow,scol){
+    res=svd(M)
+    M_low=res$u[,1:r]%*%diag(res$d[1:r],nrow=r)%*%t(res$v[,1:r])
+    step=0
+    while((srow>0)&(scol>0)){
+        row_norm=diag(M_low%*%t(M_low))
+        M_low[sort(row_norm,index=T)$ix[step+1],]=0
+        srow=srow-1
+        col_norm=diag(t(M_low)%*%M_low)
+        M_low[,sort(col_norm,index=T)$ix[step+1]]=0
+        scol=scol-1; step=step+1
+    }
+    
+    return(M_low)
+}
+
+### alternative method for simutanously low-rank+sparse model
+
+ADMM=function(X,y,r,srow,scol,rho.ini=1,p){
+    result=list();
+    n=length(X)
+    rho=rho.ini ## the larger, the better
+    
+    Lambda=matrix(0,nrow=nrow(X[[1]]),ncol=ncol(X[[1]]))
+    obj=residual=NULL
+    PQ=0; iter=0; obj = 10^10;error=10;residual=10^10
+    
+    while((iter < 100)|(error > 10^-3)){
+        ### update B
+        res=SVM_offset(X,y,C=PQ-1/rho*Lambda,p=p,cost=1/rho)
+        obj=c(obj,res$hinge) ## minimize objective
+        B=res$coef
+        
+        ## Update PQ
+        PQ=sparse_matrix(B+1/rho*Lambda,r,srow,scol)
+        
+        residual=c(residual,sum((B-PQ)^2))
+        
+        ## update Lambda
+        Lambda=Lambda+rho*(B-PQ)
+        rho=rho*1.1;
+        iter=iter+1;
+        error=abs(-residual[iter+1]+residual[iter])
+        if(iter>=200) break
+    }
+    slope = function(Xnew) sum(Xnew*B)
+    predictor = function(Xnew) sign(sum(Xnew*B)+res$intercept)
+    
+    result$alpha=res$solution;
+    result$slope=slope;result$predict=predictor;
+    result$intercept=res$intercept;
+    result$P_row=svd(PQ)$u[,1:r]; result$P_col=svd(PQ)$v[,1:r];
+    result$obj=obj[-1];result$iter=iter;
+    result$error=error;
+    result$fitted=c(res$fitted);
+    result$B=B;
+    result$residual=residual[-1];result$PQ=PQ
+    
+    return(result)
+}
+
+SVM_offset=function(X,y,C,p=0.5,cost=1){
+    offset=unlist(lapply(X,function(x)sum(x*C)))
+    n=length(X)
+    Dmat=K=matrix(unlist(X),nrow=n,byrow=TRUE)%*%t(matrix(unlist(X),nrow=n,byrow=TRUE))
+    dvec = 1-y*offset
+    Dmat = Makepositive((y%*%t(y))*Dmat)
+    Amat = cbind(y,diag(1,n),-diag(1,n))
+    
+    bvec = c(rep(0,1+n),ifelse(y==1,-cost*(1-p),-cost*p))
+    res = solve.QP(Dmat,dvec,Amat,bvec,meq =1)
+    
+    ## calculate coefficient
+    B=matrix((y*res$solution)%*%matrix(unlist(X),nrow=n,byrow=TRUE),nrow=nrow(X[[1]]))
+    coef=B+C
+    
+    ### calculate hinge loss
+    yfit=K%*%(res$solution*y)+offset
+    positive=min(yfit[y==1])
+    negative=max(yfit[y==-1])
+    if ((1-positive)<(-1-negative)) {
+        intercept = -(positive+negative)/2
+    }else{
+        gridb0 = seq(from = -1-negative,to = 1-positive,length = 100)
+        intercept = gridb0[which.min(sapply(gridb0,function(b) objective(b,yfit,y,p = p)))]
+    }
+    yfit=yfit+intercept
+    return(list("res"=res,"coef"=coef,"fitted"=yfit,"hinge"=objective(intercept,yfit,y,p=p),"intercept"=intercept))
+}
